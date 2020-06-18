d5d15b535cbd8b2d96487ce53c32c56604c6e3c9
sats17 <satishkumbhar9999@gmail.com>
2020-06-18 11:11:51 +0530

    COMMIT 1.0.1 -m Added updateRecordsHavingValueIs Function

--- JanrainInternal/UpdateRecordsInJanrain.py
+++ JanrainInternal/UpdateRecordsInJanrain.py
@@ -113,3 +113,75 @@ class UpdateRecordsInJanrain():
         }
 
         return Response
+
+    def updateRecordsHavingValueIs(self, key_attribute, update_value, recordsFileName, searchHeaderName):
+        """
+        This function will update the users, and returns two CSV files. one is users successfully updated and other is if
+        users is not found then it will be not update.
+        :param update_value:
+        :rtype: None
+        :param searchHeaderName:
+        :param recordsFileName:
+        :param key_attribute: It is the attribute name of key, e.g - email,uuid.
+        :return: CSV Files of users updated and not updated
+        """
+
+        print("Fetching records from Janrain API -> ", self.URL)
+
+        # Read data from CSV file
+        FileData = pd.read_csv(recordsFileName)
+
+        # Array for storing the Output
+        Records_Updated_Arr = []
+        Records_Not_Updated_Arr = []
+
+        # Counters
+        TotalCount = 0
+        RecordUpdatedCount = 0
+        RecordFailCount = 0
+
+        # This loop will iterate over the all CSV file data.
+        for row in FileData.itertuples():
+            searchKey = getattr(row, searchHeaderName)
+            key_value = CommonMethods.typeConverter(searchKey)
+
+            # Janrain Query params dictonary.
+            PARAMS = {'client_id': self.client_id, 'client_secret': self.client_secret, 'key_attribute': key_attribute,
+                      'key_value': key_value,
+                      'type_name': self.type_name, "attributes": json.dumps(update_value)}
+
+            # Janrain API call.
+            try:
+                response = requests.get(url=self.URL + "/entity.update", params=PARAMS).json()
+            except:
+                return "API call fail, please try again"
+
+            print("API RESPONSE --> ", response)
+            if response['stat'] == 'ok':
+                Records_Updated_Arr.append(row.key)
+                RecordUpdatedCount += 1
+            else:
+                Records_Not_Updated_Arr.append(row.key)
+                RecordFailCount += 1
+            TotalCount += 1
+            print("Total user done = ", TotalCount)
+
+        print("Generating CSV Files...")
+
+        # Writing CSV file with desired output.
+        pd.DataFrame(Records_Updated_Arr).to_csv("Janrain_records_updated.csv", index=False)
+        pd.DataFrame(Records_Not_Updated_Arr).to_csv("Janrain_records_not_updated.csv", index=False)
+
+        print("Execution Complete")
+        Response = {
+            'Total_Record_Found_In_Janrain': {
+                'Count': RecordUpdatedCount,
+                'FileName': "Janrain_records_updated.csv"
+            },
+            'Total_Record_Not_Found_In_Janrain': {
+                'Count': RecordFailCount,
+                'FileName': "Janrain_records_not_updated.csv"
+            }
+        }
+
+        return Response
